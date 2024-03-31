c6c28c7d79f2911be57cd1bd4acd5379bc3ae74c
MauricioLoya <mauloy15@gmail.com>
2024-03-31 02:17:25 -0600

    Add ua-parser-js as a dependency and update saveClick function

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { headers } from 'next/headers'
 import { findLinkByShortCode } from '../lib/links/sql/links'
 import { redirect } from 'next/navigation'
 import saveClick from '../lib/click/sql/saveClick'
+import parser from 'ua-parser-js'
 
 interface Params {
   params: { slug: string }
@@ -21,12 +22,33 @@ export default async function RedirectionPage({ params }: Params) {
   const userAgent = headers().get('user-agent') ?? undefined
   const referer = headers().get('referer') ?? undefined
   const ip = (headers().get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+
+  const userAgenetParsed = parser(userAgent)
+  const browser = userAgenetParsed.browser.name
+  const browserVersion = userAgenetParsed.browser.version
+  const os = userAgenetParsed.os.name
+  const osVersion = userAgenetParsed.os.version
+  const deviceVendor = userAgenetParsed.device.vendor
+  const deviceModel = userAgenetParsed.device.model
+  const deviceType = userAgenetParsed.device.type
+  const architecture = userAgenetParsed.cpu.architecture
+
   try {
     await saveClick({
       linkId: link.id,
       userAgent,
       referer,
-      ip
+      ip,
+      userAgentDetail: {
+        browser,
+        browserVersion,
+        os,
+        osVersion,
+        deviceVendor,
+        deviceModel,
+        deviceType,
+        architecture
+      }
     })
   } catch (error) {
     console.log('error', error)
--- src/app/lib/click/sql/saveClick.ts
+++ src/app/lib/click/sql/saveClick.ts
@@ -5,13 +5,49 @@ interface DtoSaveClick {
   userAgent?: string
   referer?: string
   ip?: string
+  userAgentDetail?: {
+    browser?: string
+    browserVersion?: string
+    os?: string
+    osVersion?: string
+    deviceVendor?: string
+    deviceModel?: string
+    deviceType?: string
+    architecture?: string
+  }
 }
 
 export default async function saveClick(dto: DtoSaveClick): Promise<number> {
   try {
     const result = await sql`
-              INSERT INTO clicks (ipaddress, referrer, useragent, linkid)
-              VALUES (${dto.ip}, ${dto.referer}, ${dto.userAgent}, ${dto.linkId})`
+              INSERT INTO clicks (
+                ipaddress,
+                referrer,
+                useragent,
+                linkid,
+                browser,
+                browserversion,
+                os,
+                osversion,
+                devicevendor,
+                devicemodel,
+                devicetype,
+                architecture
+                )
+                VALUES (
+                    ${dto.ip},
+                    ${dto.referer},
+                    ${dto.userAgent},
+                    ${dto.linkId},
+                    ${dto.userAgentDetail?.browser},
+                    ${dto.userAgentDetail?.browserVersion},
+                    ${dto.userAgentDetail?.os},
+                    ${dto.userAgentDetail?.osVersion},
+                    ${dto.userAgentDetail?.deviceVendor},
+                    ${dto.userAgentDetail?.deviceModel},
+                    ${dto.userAgentDetail?.deviceType},
+                    ${dto.userAgentDetail?.architecture}
+                )`
     return result.oid
   } catch (error) {
     throw error
