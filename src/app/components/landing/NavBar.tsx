885a1abde816c6d4a0df4dbffb7ab72cdffd015f
MauricioLoya <mauloy15@gmail.com>
2024-04-02 15:12:38 -0600

    Update NavBar link to use relative path

--- src/app/components/landing/NavBar.tsx
+++ src/app/components/landing/NavBar.tsx
@@ -8,7 +8,7 @@ export default async function NavBar() {
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
-          href="https://flowbite.com/"
+          href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img src="logo.png" className="dark:hidden h-12" alt="daLink Logo" />
