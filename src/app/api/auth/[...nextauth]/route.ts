5978d9916c405ac5685dbc7e810bdc14d2b9667f
Mauricio Loya <mauloy15@hotmail.com>
2025-03-07 10:21:56 -0600

    Comment out Github authentication provider in NextAuth configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth, { Account, Profile, User } from 'next-auth'
 // import Discord from 'next-auth/providers/discord'
-import GithubProvider from 'next-auth/providers/github'
+// import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import { createUserByProvider, findUserByEmail } from '@/app/lib/users/sql/user'
 
@@ -12,10 +12,10 @@ interface SignInArgs {
 const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID ?? '',
+    //   clientSecret: process.env.GITHUB_SECRET ?? ''
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? '',
       clientSecret: process.env.GOOGLE_SECRET ?? ''
